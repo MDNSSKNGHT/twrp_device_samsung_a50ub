922abeebe4f2027ec221c22b3a11296697099017
Leonardo Estacio <artec.145@hotmail.com>
2021-12-05 13:30:11 -0500

    Initial bootable device tree for SM-A505G (Galaxy A50)
    Signed-off-by: Leonardo Estacio <artec.145@hotmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,88 @@
+#
+# Copyright 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This contains the module build definitions for the hardware-specific
+# components for this device.
+#
+# As much as possible, those components should be built unconditionally,
+# with device-specific names to avoid collisions, to avoid device-specific
+# bitrot and build breakages. Building a component unconditionally does
+# *not* include it on all devices, so it is safe even with hardware-specific
+# components.
+
+DEVICE_PATH := device/samsung/a50ub
+
+# Platform
+TARGET_BOARD_PLATFORM := exynos9610
+TARGET_BOARD_PLATFORM_GPU := mali-g72
+TARGET_USES_64_BIT_BINDER := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a73
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := exynos9610
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_NAME := SRPRL28A009KU
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x10000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_HEADER_VERSION := 1
+
+BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --board $(BOARD_NAME)
+
+# Prebuilt: Kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
+BOARD_KERNEL_IMAGE_NAME := Image
+
+# Prebuilt: DTBO
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+
+# SAR
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+# SPL
+PLATFORM_SECURITY_PATCH := 2021-10
+PLATFORM_VERSION := 11.0.0
+
+# TWRP
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
+TW_NO_REBOOT_BOOTLOADER := true # Whether to hide "Bootloader" in the reboot menu.
+TW_HAS_DOWNLOAD_MODE := true # Whether to show "Download mode" in the reboot menu.
